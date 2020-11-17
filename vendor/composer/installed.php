b5771807b11d7c3691a1ac52f8230517b259cc55
carlos-palalo <carlos-palalo@gmail.com>
2020-11-17 16:23:59 +0100

    Librerias composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,42 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '100ebb00ac9c32afdd309e8329531d2ed753cf22',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '100ebb00ac9c32afdd309e8329531d2ed753cf22',
+    ),
+    'ballen/distical' => 
+    array (
+      'pretty_version' => '2.0.7',
+      'version' => '2.0.7.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b9383462e68fa0ce6f2be4a51ba8777545e82fe5',
+    ),
+    'sujalpatel/inttoenglish' => 
+    array (
+      'pretty_version' => 'v1.0.4',
+      'version' => '1.0.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9704d0c2051cb63f6c4a35e42adc80081cd309f6',
+    ),
+  ),
+);
